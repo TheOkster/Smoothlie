8fb8663160dfe4e12e9141211684e67d02b81b7e
Isha Agarwal <isha.agarwal3139@gmail.com>
2024-01-19 19:51:47 -0500

    tried to implement dnd exmaple - not working rn

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -59,7 +59,7 @@ const App = () => {
         />
         <Route path="*" element={<NotFound />} />
       </Routes>
-      <DragndDrop />
+      {/* <DragndDrop /> */}
     </>
   );
 };
--- client/src/components/DragndDrop.js
+++ client/src/components/DragndDrop.js
@@ -1,7 +1,23 @@
 import React, { useState, useEffect } from "react";
+import { DndContext } from "@dnd-kit/core";
+
+import Draggable from "./Draggable";
+import Droppable from "./Droppable";
 
 const DragndDrop = () => {
-  return <div>hi this is my drag and drop component (to be implemented) </div>;
+  const [parent, setParent] = useState(null);
+  const draggable = <Draggable id="draggable">Go ahead, drag me</Draggable>;
+
+  const handleDragEnd = (over) => {
+    setParent(over ? over.id : null);
+  };
+
+  return (
+    <DndContext onDragEnd={handleDragEnd}>
+      {!parent ? draggable : null}
+      <Droppable id="droppable">{parent === "droppable" ? draggable : "Drop here"}</Droppable>
+    </DndContext>
+  );
 };
 
 export default DragndDrop;
