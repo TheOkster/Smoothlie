f0fbd51a8046f174c4741027f6cec41555368e34
TheOkster <136392062+TheOkster@users.noreply.github.com>
2024-01-19 22:10:30 -0500

    Changed dropdown class name

--- client/src/components/modules/Dropdown.js
+++ client/src/components/modules/Dropdown.js
@@ -4,16 +4,19 @@ import React, { useState, useEffect } from "react";
 
 /**
  * Component to render an online user
- *
  * Proptypes
  *  *  sets it to active
- * @param {Array[String]} fields
+ * @property {string} _id
+ * @property {string} _name
+ *  @param {string} label
+ *  @param {Array[String]} fields
  */
 const Dropdown = (props) => {
   return (
-    <div className="dropdown">
-      <label for="dog-names">Choose a dog name:</label>
-      <select name="dog-names" id="dog-names">
+    // Uses Builtin OS dropdown, can change later for better design
+    <div className="Dropdown-dropdown">
+      <label for={props._id}>{props.label}</label>
+      <select name={props._name} id={props._id}>
         {props.fields.map((field) => (
           <option value={field}>{field}</option>
         ))}
