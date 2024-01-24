8483ec59ed2486d5b6295b5c2c0549cb6da24978
TheOkster <136392062+TheOkster@users.noreply.github.com>
2024-01-24 13:09:06 -0500

    Added optional parameter handleTaskLit to UrgentImp

--- client/src/components/pages/Grid.js
+++ client/src/components/pages/Grid.js
@@ -5,10 +5,11 @@ import UrgentImp from "../modules/UrgentImp.js";
 const GridPage = () => {
   const location = useLocation();
   const [taskList, setTaskList] = useState(location.state.taskList);
+  const [quadrants, setQuadrants] = useState();
   // console.log(location.state.taskList);
   return (
     <>
-      <UrgentImp taskList={taskList} />
+      <UrgentImp taskList={taskList} handleTaskList={(event) => setQuadrants(event)} />
     </>
   );
 };
