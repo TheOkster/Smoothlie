a0c46d4dbd92a91d69968fbe2c92e135779a53d2
Isha Agarwal <isha.agarwal3139@gmail.com>
2024-01-28 17:27:07 -0500

    added new event schema and updated pastsmoothies.js page so now you can see the events of your past smoothies on your calendar

--- server/models/smoothie.js
+++ server/models/smoothie.js
@@ -1,17 +1,15 @@
 const mongoose = require("mongoose");
 
+const EventSchema = new mongoose.Schema({
+  title: String,
+  start: Date,
+  end: Date,
+});
+
 const SmoothieSchema = new mongoose.Schema({
   name: String,
   owner: String,
-  /* Each timestamp represents the time it includes and 15 minutes ahead
-     May change to maek it easier to understand */
-  timestamps: [],
-  events: [
-    {
-      startTimestamp: Number,
-      endTimestamp: Number,
-    },
-  ],
+  events: [EventSchema],
 });
 // compile model from schema
 module.exports = mongoose.model("smoothie", SmoothieSchema);
