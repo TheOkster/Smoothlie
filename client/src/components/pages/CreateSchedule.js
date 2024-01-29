5d4d8d8b3ed99f3a373b8b6a34aced4b58c5b1d5
Isha Agarwal <isha.agarwal3139@gmail.com>
2024-01-28 22:05:04 -0500

    created customizable props for entering availability... entering the min/max time is still funky and needs to be fixed

--- client/src/components/pages/CreateSchedule.js
+++ client/src/components/pages/CreateSchedule.js
@@ -11,11 +11,9 @@ const CreateSchedule = (props) => {
   const [startDate, setStartDate] = useState(new Date());
   const [numDays, setNumDays] = useState(7);
   // const [hourlyChunks, setHourlyChunks] = useState(4); - need to add functionality in algorithm to do this
-  const [minTime, setMinTime] = useState(11);
-  const [maxTime, setMaxTime] = useState(20);
+  const [minTime, setMinTime] = useState(9);
+  const [maxTime, setMaxTime] = useState(17);
   const [taskList, setTaskList] = useState(location.state.taskList);
-
-  console.log(`task list in createschedule ${JSON.stringify(taskList)}`);
 
   const handleClick = (event) => {
     navigate("/result", {
@@ -26,26 +24,65 @@ const CreateSchedule = (props) => {
     });
   };
 
-  const handleChange = (newSchedule) => {
+  const handleChange = (setter) => {
+    console.log("i am in the handleChange function");
+    return (event) => {
+      if (event.target.value !== "") {
+        setter(event.target.value);
+      }
+    };
+  };
+
+  const handleScheduleChange = (newSchedule) => {
+    console.log("in the handle schedule change function");
+    console.log(newSchedule);
     setAvailable(newSchedule);
   };
 
   // to do: add functionality for user to enter startdate, numdays, mintime, maxtime
   return (
     <>
-      {/* //   <div>
-    //     <ScheduleSelector
-    //       minTime={minTime}
-    //       maxTime={maxTime}
-    //       numDays={numDays}
-    //       startDate={new Date(startDate)}
-    //       selection={available}
-    //       onChange={handleChange}
-    //       hourlyChunks={2}
-    //       timeFormat="h:mma"
-    //     />
-    //   </div> */}
-      <EnterSchedule available={available} setAvailable={setAvailable} />
+      <div>
+        Start Date:
+        <input type="date" defaultValue={new Date()} onChange={handleChange(setStartDate)} />
+        Number of Days:
+        <input
+          type="number"
+          min="1"
+          max="1000"
+          value={numDays}
+          onChange={handleChange(setNumDays)}
+        />
+        Min Hour:
+        <input
+          type="number"
+          min="0"
+          max={maxTime - 1}
+          value={minTime}
+          onChange={handleChange(setMinTime)}
+        />
+        Max Hour:
+        <input
+          type="number"
+          min={minTime + 1}
+          max="24"
+          value={maxTime}
+          onChange={handleChange(setMaxTime)}
+        />
+      </div>
+      <div>
+        <ScheduleSelector
+          minTime={minTime}
+          maxTime={maxTime}
+          numDays={numDays}
+          startDate={startDate}
+          selection={available}
+          onChange={setAvailable}
+          hourlyChunks={4}
+          timeFormat="h:mma"
+        />
+      </div>
+      {/* <EnterSchedule available={available} setAvailable={setAvailable} /> */}
       <button onClick={handleClick}>Submit Availability</button>
     </>
   );
