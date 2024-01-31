e5aa7b914b5474f36f2dd8247c767dcd11039849
TheOkster <136392062+TheOkster@users.noreply.github.com>
2024-01-31 10:46:40 -0500

    Added Pacifico font onto Homepage, but text is still not the same size

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { GoogleOAuthProvider, GoogleLogin, googleLogout } from "@react-oauth/google";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import "../../utilities.css";
 import "./Home.css";
@@ -13,42 +13,51 @@ const GOOGLE_CLIENT_ID =
 const Home = ({ userId, handleLogin, handleLogout }) => {
   return (
     <>
+      <head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <div className="Background">
-            {/* <img src={require("./TaskPage.jpg").default} style={{ width: "100%", height: "100%", objectFit: "cover", objectPosition: "center"}} /> */}
+        {/* <img src={require("./TaskPage.jpg").default} style={{ width: "100%", height: "100%", objectFit: "cover", objectPosition: "center"}} /> */}
         <div className="Home-container">
           <div className="Home-logo">
             <h1>Smooth</h1>
-            <h1><div className="Home-l">l</div></h1>
+            <h1>
+              <div className="Home-l">l</div>
+            </h1>
             <h1>ie</h1>
-            </div>
+          </div>
           <div className="buttonContainer">
-          <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
-            {userId ? (
-              <>
-              <Link to={{pathname: "/selecttasks"}}> {/* need to fix this later; this is going to redirect to another page depending on whether you've selected tasks before*/}
-                <button className="Button">
-                  Welcome! Click here to begin.
-                </button>
-              </Link>
+            <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+              {userId ? (
+                <>
+                  <Link to={{ pathname: "/selecttasks" }}>
+                    {" "}
+                    {/* need to fix this later; this is going to redirect to another page depending on whether you've selected tasks before*/}
+                    <button className="Button">Welcome! Click here to begin.</button>
+                  </Link>
 
-              <button className ="Button"
-                onClick={() => {
-                  googleLogout();
-                  handleLogout();
-                }}
-              >
-                Logout
-              </button>
-              </>
-            ) : (
-              <>
-              <div className="Button">
-                <GoogleLogin onSuccess={handleLogin} onError={(err) => console.log(err)} />
-              </div>
-              <div className="Label">Please login to start!</div>
-              </>
-            )}
-          </GoogleOAuthProvider>
+                  <button
+                    className="Button"
+                    onClick={() => {
+                      googleLogout();
+                      handleLogout();
+                    }}
+                  >
+                    Logout
+                  </button>
+                </>
+              ) : (
+                <>
+                  <div className="Button">
+                    <GoogleLogin onSuccess={handleLogin} onError={(err) => console.log(err)} />
+                  </div>
+                  <div className="Label">Please login to start!</div>
+                </>
+              )}
+            </GoogleOAuthProvider>
           </div>
         </div>
       </div>
